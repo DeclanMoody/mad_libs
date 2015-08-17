0e6cffb844d964c5637905c4aa7369b3474c995c
Declan M. Moody <declanmmoody@gmail.com>
2015-08-17 04:26:23 +0000

    added second madlib and play again

--- Mad_Libs.rb
+++ Mad_Libs.rb
@@ -1,4 +1,5 @@
 class Mad_Libs
+
   def initialize
     choose_madlibs
   end
@@ -8,6 +9,26 @@ class Mad_Libs
     @number = gets.chomp.to_i
     different_madlibs
   end
+
+  def if_answer
+      if @answer == "yes"
+        choose_madlibs
+      elsif @answer == "no"
+        puts "Thats to bad"
+        exit
+      else
+        puts "thats not an answer"
+        again
+      end
+    end
+
+    def play_again
+      puts ""
+      puts ""
+      print "Do you want to play again : "
+      @answer = gets.chomp.downcase
+      if_answer
+    end
 
   def different_madlibs
     if @number == 1
@@ -39,7 +60,30 @@ class Mad_Libs
       puts ""
       puts "You may think that this is the #{@noun_3},"
       puts "Well it is."
-
+      play_again
+    elsif @number == 2
+      print "Pick a adjective : "
+      @adjective = gets.chomp
+      puts ""
+      print "Pick a noun : "
+      @noun = gets.chomp
+      puts ""
+      print "Pick an animal : "
+      @animal = gets.chomp
+      print "Pick a sound : "
+      @sound = gets.chomp.capitalize
+      puts ""
+      puts "Here is your mad libs : "
+      puts ""
+      puts ""
+      puts "#{@adjective} Macdonald had a #{@noun}, E-I-E-I-O"
+      puts "and on that #{@noun} he had an #{@animal}, E-I-E-I-O"
+      puts "with a #{@sound} #{@sound} here"
+      puts "and a #{@sound} #{@sound} there"
+      puts "here a #{@sound}, there a #{@sound}"
+      puts "everywhere a #{@sound} #{@sound},"
+      puts "#{@adjective} Macdonald had a #{@noun}, E-I-E-I-O"
+      play_again
     else
      puts "that's not an answer"
       choose_madlibs

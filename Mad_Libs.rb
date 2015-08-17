5deb8f6c17448ee4731c5dbb2eaa4ab1770ca749
Declan M. Moody <declanmmoody@gmail.com>
2015-08-17 02:33:27 +0000

    made first mad lib

--- Mad_Libs.rb
+++ Mad_Libs.rb
@@ -11,11 +11,38 @@ class Mad_Libs
 
   def different_madlibs
     if @number == 1
-      print "Pick a noun"
-      noun = gets.chomp
-      print "Pick a plural noun"
-      plural noun = gets.chomp
-      print
+      print "Pick a noun : "
+      @noun = gets.chomp
+      puts ""
+      print "Pick a plural noun : "
+      @plural_noun = gets.chomp
+      puts ""
+      print "Pick another noun : "
+      @noun_2 = gets.chomp
+      puts ""
+      print "Pick a place : "
+      @place = gets.chomp
+      puts ""
+      print "Pick an adjective : "
+      @adjective = gets.chomp
+      puts ""
+      print "Pick another noun : "
+      @noun_3 = gets.chomp
+      puts ""
+      puts "Here is your mad libs : "
+      puts ""
+      puts ""
+      puts "Be kind to your #{@noun}-footed #{@plural_noun}"
+      puts "For a duck may be somebody`s #{@noun_2},"
+      puts "Be kind to your #{@plural_noun} in #{@place}"
+      puts "Where the weather is always #{@adjective}."
+      puts ""
+      puts "You may think that this is the #{@noun_3},"
+      puts "Well it is."
+
+    else
+     puts "that's not an answer"
+      choose_madlibs
     end
   end
 end
